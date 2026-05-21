8a85cbb56699ba3297d364351da6464a0c5decac
Rhodium Music Tech <admin@rhodiummusic.com>
2026-05-21 16:00:38 +0300

    chore: update build outputs and package-lock post-merge

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,3 +1,3 @@
-export const BUILD_ID = "2.7.0-1776975562123";
+export const BUILD_ID = "2.7.0-1779368380016";
 export const BUILD_VERSION = "2.7.0";
-export const BUILD_TIMESTAMP = "2026-04-23T20:19:22.123Z";
+export const BUILD_TIMESTAMP = "2026-05-21T12:59:40.016Z";
